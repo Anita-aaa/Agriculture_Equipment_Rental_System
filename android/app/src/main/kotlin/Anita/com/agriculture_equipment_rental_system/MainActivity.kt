b2241b10e38e609470f7f195ff16aa1cdc41f002
Anita <at9941787@gmail.com>
2024-12-12 21:57:46 +0545

    Project setup

---
+++ android/app/src/main/kotlin/Anita/com/agriculture_equipment_rental_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package Anita.com.agriculture_equipment_rental_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:agriculture_equipment_rental_system/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
