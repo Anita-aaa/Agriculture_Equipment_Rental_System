e81bfd4bba301289415eb25fff5cd465189b227c
Anita <at9941787@gmail.com>
2024-12-13 14:13:21 +0545

    Register Page

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 // import 'package:agriculture_equipment_rental_system/view/login_view.dart';
-import 'package:agriculture_equipment_rental_system/view/onboarding_screen_view.dart';
+// import 'package:agriculture_equipment_rental_system/view/onboarding_screen_view.dart';
+import 'package:agriculture_equipment_rental_system/view/registration_view.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,6 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: OnboardingPage());
+        debugShowCheckedModeBanner: false, home: RegistrationView());
   }
 }
