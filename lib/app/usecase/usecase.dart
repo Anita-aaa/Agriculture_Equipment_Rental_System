cfd4af226e5f311a504d1674d4b0fd109a4451b3
Anita <at9941787@gmail.com>
2025-01-17 12:15:00 +0545

    UsecaseWithParams

---
+++ lib/app/usecase/usecase.dart
@@ -0,0 +1,10 @@
+import 'package:agriculture_equipment_rental_system/core/error/falure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
